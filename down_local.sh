45f14eaabe8484604d1ce790776cff4ff573dadf
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-10-28 00:31:50 -0400

    Up and Down scripts.
    Up and Down scripts.

---
+++ down_local.sh
@@ -0,0 +1,2 @@
+sudo docker stop task-nest
+sudo docker remove task-nest
--- up_local.sh
+++ up_local.sh
@@ -1,5 +1,5 @@
 # Ensure docker is installed and running
 echo Running the following command: docker build -t task-nest .
 sudo docker build -t task-nest .
-echo "Running the following command: docker run -d -p 3000:3000 task-nest"
-sudo docker run -d -p 3000:3000 task-nest
+echo "Running the following command: docker run -d -p 3000:3000 --name task-nest task-nest"
+sudo docker run -d -p 3000:3000 --name task-nest task-nest
