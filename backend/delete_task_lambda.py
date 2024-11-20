50f6df1917202db405016d69692b2fa40a49eb74
Cole Aydelotte <aydelottec@moravian.edu>
2024-11-20 11:33:12 -0500

    Added delete_task_lambda.py
    Added delete_task_lambda.py

---
+++ backend/delete_task_lambda.py
@@ -0,0 +1,59 @@
+import json
+import boto3
+def lambda_handler(event, context):
+
+    class DynamoDB:
+        def __init__(self):
+            self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+            self.table = self.dynamodb.Table('task-nest-users')
+
+        def get_tasks(self, user_id):
+            """Retrieve tasks for a specific user from DynamoDB."""
+            response = self.table.get_item(Key={'user-id': user_id})
+            if 'Item' not in response:
+                return []
+            
+            tasks = response['Item'].get('tasks', [])
+            print(tasks)
+            return tasks
+
+    def delete_task(self, user_id, description):
+        """Delete a specific task by description for a user."""
+        tasks = self.get_tasks(user_id)
+        updated_tasks = [task for task in tasks if task.get("description") != description]
+        if len(tasks) == len(updated_tasks):
+            return {"error": "Task not found."}
+
+        response = self.table.update_item(
+            Key={'user-id': user_id},
+            UpdateExpression="SET tasks = :tasks",
+            ExpressionAttributeValues={':tasks': updated_tasks},
+            ReturnValues="UPDATED_NEW"
+        )
+        return response
+
+    body = json.loads(event['body'])
+
+    db_connection = DynamoDB()
+
+    user_id =  body.get('user_id')
+    description = body.get('description')
+    if not user_id or not description:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"error": "User ID and description are required"})
+        }
+
+    response = db_connection.delete_task(user_id, description)
+    
+    if "error" in response:
+        return {
+            'statusCode': 404,
+            'body': json.dumps(response)
+        }
+    
+    updated_tasks = db_connection.get_tasks(user_id)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(updated_tasks)
+    }
