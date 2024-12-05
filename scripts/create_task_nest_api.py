34380c486c2b4eb5da00804eac223053762a2141
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-12-04 23:52:57 -0500

    Working on API script.
    Working on API script.

--- scripts/create_task_nest_api.py
+++ scripts/create_task_nest_api.py
@@ -1,93 +1,353 @@
-import boto3
+import boto3, json
 import sys
 
-# Creating connections to the various services
 client = boto3.client('apigateway', region_name='us-east-1')
-lambda_client = boto3.client('lambda', region_name='us-east-1')
-iam_client = boto3.client('iam')
 
 response = client.get_rest_apis()
 apis = response.get('items', [])
-
-# Only continues if API doesn't exist
+        
 for api in apis:
-    if api.get('name') == 'task_nest_api':
-        print('API already exists')
+    if api.get('name') == 'task_nest_rest_api':
+        print('API already exits')
         sys.exit(0)
 
+
 response = client.create_rest_api(
-    name='task_nest_api',
-    description='API to manage tasks',
-    endpointConfiguration={'types': ['REGIONAL']}
+    name='task_nest_rest_api',
+    description='API to tally votes.',
+    endpointConfiguration={
+        'types': [
+            'REGIONAL',
+        ]
+    }
 )
 api_id = response["id"]
 
 resources = client.get_resources(restApiId=api_id)
 root_id = [resource for resource in resources["items"] if resource["path"] == "/"][0]["id"]
 
+import boto3
+
+cognito_client = boto3.client('cognito-idp', region_name='us-east-1')
+api_gateway_client = boto3.client('apigateway', region_name='us-east-1')
+sts_client = boto3.client('sts')
+
+account_id = sts_client.get_caller_identity()['Account']
+region = 'us-east-1'
+
+response = cognito_client.list_user_pools(MaxResults=10)
+user_pools = response.get('UserPools', [])
+
+if not user_pools:
+    raise Exception("No user pools found in this account.")
+
+user_pool_id = next(pool['Id'] for pool in user_pools if pool['Name'] == 'task-nest-user-pool')
+
+if user_pool_id is None:
+    pass # Create the user pool
+
+cognito_user_pool_arn = f"arn:aws:cognito-idp:{region}:{account_id}:userpool/{user_pool_id}"
+
+authorizer = api_gateway_client.create_authorizer(
+    restApiId=api_id,
+    name='task_nest_authorizer',
+    type='COGNITO_USER_POOLS',
+    providerARNs=[cognito_user_pool_arn],
+    identitySource='method.request.header.Authorization'
+)
+
+authorizer_id = authorizer['id']
+print(f"Authorizer created with ID: {authorizer_id}")
+
+
+# Create the /tasks resource
+tasks = client.create_resource(
+    restApiId=api_id,
+    parentId=root_id,
+    pathPart='tasks'
+)
+tasks_resource_id = tasks["id"]
+
+tasks_method = client.put_method(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='GET',
+    authorizationType='COGNITO_USER_POOLS',
+    authorizerId=authorizer_id
+)
+
+tasks_response = client.put_method_response(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='GET',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': True,
+        'method.response.header.Access-Control-Allow-Origin': True,
+        'method.response.header.Access-Control-Allow-Methods': True
+    },
+    responseModels={
+        'application/json': 'Empty'
+    }
+)
+
+tasks_integration_response = client.put_integration_response(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='GET',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+        'method.response.header.Access-Control-Allow-Methods': '\'GET\'.\'OPTIONS\'',
+        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+    }
+)
+
+lambda_client = boto3.client('lambda', region_name='us-east-1')
+lambda_arn=lambda_client.get_function(FunctionName='get_tasks')['Configuration']['FunctionArn']
+uri=f'arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_arn}/invocations'
+
+if lambda_arn is None:
+    raise Exception("No Lambda function found, make sure lambda function get_tasks exists")
+
+iam_client = boto3.client('iam')
 lab_role = iam_client.get_role(RoleName='LabRole')['Role']['Arn']
 
-def create_method(client, api_id, resource_id, method, lambda_function_name, is_mock=False):
-    """
-    Create a method for a resource in an API Gateway.
-    Created this because script was getting very lengthy and since its python,
-    we can just use a function to reduce the boilerplate code.
-    """
-    client.put_method(
-        restApiId=api_id,
-        resourceId=resource_id,
-        httpMethod=method,
-        authorizationType='NONE'
-    )
+tasks_integration = client.put_integration(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='GET',
+    credentials=lab_role,
+    integrationHttpMethod='POST',
+    type='AWS_PROXY',
+    uri=uri,
+    requestTemplates={
+        "application/json": '{ "queryStringParameters": { "user_id": "$input.params(\'user_id\')" } }'
+    }
+)
 
-    client.put_method_response(
-        restApiId=api_id,
-        resourceId=resource_id,
-        httpMethod=method,
-        statusCode='200',
-        responseParameters={
-            'method.response.header.Access-Control-Allow-Headers': True,
-            'method.response.header.Access-Control-Allow-Origin': True,
-            'method.response.header.Access-Control-Allow-Methods': True
-        },
-        responseModels={'application/json': 'Empty'}
-    )
+# Now /tasks preflight
+tasks_method = client.put_method(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='OPTIONS',
+    authorization='NONE'
+    Type='NONE'
+)
 
-    if is_mock:
-        client.put_integration(
-            restApiId=api_id,
-            resourceId=resource_id,
-            httpMethod=method,
-            type='MOCK',
-            requestTemplates={'application/json': '{"statusCode": 200}'}
-        )
-    else:
-        lambda_arn = lambda_client.get_function(FunctionName=lambda_function_name)['Configuration']['FunctionArn']
-        uri = f'arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_arn}/invocations'
+tasks_response = client.put_method_response(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': True,
+        'method.response.header.Access-Control-Allow-Origin': True,
+        'method.response.header.Access-Control-Allow-Methods': True
+    },
+    responseModels={
+        'application/json': 'Empty'
+    }
+)
 
-        client.put_integration(
-            restApiId=api_id,
-            resourceId=resource_id,
-            httpMethod=method,
-            credentials=lab_role,
-            integrationHttpMethod='POST',
-            type='AWS_PROXY',
-            uri=uri
-        )
+tasks_integration = client.put_integration(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='OPTIONS',
+    type='MOCK',
+    requestTemplates={
+        'application/json': '{"statusCode": 200}'
+    }
+)
 
-# /tasks
-tasks = client.create_resource(restApiId=api_id, parentId=root_id, pathPart='tasks')
-create_method(client, api_id, tasks["id"], 'GET', 'get_tasks')
-create_method(client, api_id, tasks["id"], 'OPTIONS', 'get_tasks', is_mock=True)
+tasks_integration_response = client.put_integration_response(
+    restApiId=api_id,
+    resourceId=tasks_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+        'method.response.header.Access-Control-Allow-Methods': '\'GET\',\'OPTIONS\'',
+        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+    }
+)
 
-# /add
-add_resource = client.create_resource(restApiId=api_id, parentId=root_id, pathPart='add')
-create_method(client, api_id, add_resource["id"], 'POST', 'add_task')
-create_method(client, api_id, add_resource["id"], 'OPTIONS', 'add_task', is_mock=True)
+# Create the /add resource
+add_resource = client.create_resource(
+    restApiId=api_id,
+    parentId=root_id,
+    pathPart='add'
+)
+add_resource_id = add_resource["id"]
 
-# /delete
-delete_resource = client.create_resource(restApiId=api_id, parentId=root_id, pathPart='delete')
-create_method(client, api_id, delete_resource["id"], 'DELETE', 'delete_task')
-create_method(client, api_id, delete_resource["id"], 'OPTIONS', 'delete_task', is_mock=True)
+add_method = client.put_method(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='POST',
+    authorizationType='COGNITO_USER_POOLS',
+    authorizerId=authorizer_id
+)
 
-print("DONE")
+add_response = client.put_method_response(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='POST',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': True,
+        'method.response.header.Access-Control-Allow-Origin': True,
+        'method.response.header.Access-Control-Allow-Methods': True
+    },
+    responseModels={
+        'application/json': 'Empty'
+    }
+)
+
+add_integration_response = client.put_integration_response(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='POST',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+        'method.response.header.Access-Control-Allow-Methods': '\'POST\'.\'OPTIONS\'',
+        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+    }
+)
+
+lambda_arn=lambda_client.get_function(FunctionName='add_task')['Configuration']['FunctionArn']
+uri=f'arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/{lambda_arn}/invocations'
+
+if lambda_arn is None:
+    raise Exception("No Lambda function found, make sure lambda function add_task exists")
+
+add_integration = client.put_integration(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='POST',
+    credentials=lab_role,
+    integrationHttpMethod='POST',
+    type='AWS_PROXY',
+    uri=uri,
+    requestTemplates={
+        "application/json": '{ "body": $input.json("$") }'
+    }
+)
+
+# Now /add preflight
+
+add_method = client.put_method(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='OPTIONS',
+    authorization='NONE',
+    Type='NONE'
+)
+
+add_response = client.put_method_response(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': True,
+        'method.response.header.Access-Control-Allow-Origin': True,
+        'method.response.header.Access-Control-Allow-Methods': True
+    },
+    responseModels={
+        'application/json': 'Empty'
+    }
+)
+
+add_integration = client.put_integration(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='OPTIONS',
+    type='MOCK',
+    requestTemplates={
+        'application/json': '{"statusCode": 200}'
+    }
+)
+
+add_integration_response = client.put_integration_response(
+    restApiId=api_id,
+    resourceId=add_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+        'method.response.header.Access-Control-Allow-Methods': '\'POST\',\'OPTIONS\'',
+        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+    }
+)
+
+# Create the /delete resource
+
+#vote_integration_response = client.put_integration_response(
+#    restApiId=api_id,
+#    resourceId=vote_resource_id,
+##    httpMethod='POST',
+#    statusCode='200',
+#    responseParameters={
+#        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+#        'method.response.header.Access-Control-Allow-Methods': '\'POST\'.\'OPTIONS\'',
+#        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+#    },
+#    responseTemplates={
+#        "application/json": json.dumps({
+#            "yes": 20,
+#            "no": 10               
+#        })
+#    }
+#)
+
+
+vote_method = client.put_method(
+    restApiId=api_id,
+    resourceId=vote_resource_id,
+    httpMethod='OPTIONS',
+    authorizationType='NONE'
+)
+
+vote_response = client.put_method_response(
+    restApiId=api_id,
+    resourceId=vote_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': False,
+        'method.response.header.Access-Control-Allow-Origin': False,
+        'method.response.header.Access-Control-Allow-Methods': False
+    },
+    responseModels={
+        'application/json': 'Empty'
+    }
+)
+
+
+vote_integration = client.put_integration(
+    restApiId=api_id,
+    resourceId=vote_resource_id,
+    httpMethod='OPTIONS',
+    type='MOCK',
+    requestTemplates={
+        'application/json': '{"statusCode": 200}'
+    }
+)
+
+
+vote_integration_response = client.put_integration_response(
+    restApiId=api_id,
+    resourceId=vote_resource_id,
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token\'',
+        'method.response.header.Access-Control-Allow-Methods': '\'POST\',\'OPTIONS\'',
+        'method.response.header.Access-Control-Allow-Origin': '\'*\''
+    }
+)
+
+
+
+print ("DONE")
